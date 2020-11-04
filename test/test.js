088d14d0e53e6695f6865619ee6f5678fec034ed
Jonathan Keppler <JKeppler27@gmail.com>
2020-11-04 16:52:12 -0500

    feat(security-rules): add more tests for security rules

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 const assert = require('assert');
 const firebase = require('@firebase/testing');
+const { isContext } = require('vm');
 
 const MY_PROJECT_ID = "expense-buddy-8c526";
 const myId = "user_abc";
@@ -66,19 +67,139 @@ describe("Expense Buddy", () => {
         await firebase.assertFails(testRead.get());
     });
 
-    it("Can create a new expenseType if user is logged in", async() => {
-        const db = getFirestore(myAuth);
-        const testDoc = db.collection("expenseTypes").doc("test_doc");
-        await firebase.assertSucceeds(testDoc.set({foo: "bar"}));
-    });
-
     it("Can't create a new expenseType if user is not logged in", async() => {
         const db = getFirestore(null);
         const testDoc = db.collection("expenseTypes").doc("test_doc");
         await firebase.assertFails(testDoc.set({foo: "bar"}));
     });
 
+    it("Can't create a new expenseType if user is logged in but not part of scopedUsers", async() => {
+        const db = getFirestore(myAuth);
+        const testDoc = db.collection("expenseTypes").doc("test_doc");
+        await firebase.assertFails(testDoc.set({scopedUsers: [theirId]}));
+    });
 
+    it("Can't create a new expenseType if user is not logged in but part of scopedUsers", async() => {
+        const db = getFirestore(null);
+        const testDoc = db.collection("expenseTypes").doc("test_doc");
+        await firebase.assertFails(testDoc.set({scopedUsers: [myId]}));
+    });
+
+    it("Can create a new expenseType if user is logged in and part of scopedUsers", async() => {
+        const db = getFirestore(myAuth);
+        const testDoc = db.collection("expenseTypes").doc("test_doc");
+        await firebase.assertSucceeds(testDoc.set({scopedUsers: [myId]}));
+    });
+
+    /*****************
+     **  SUB TYPES  **
+     *****************/
+    it("Can read subTypes if parent expenseType is not scoped and subType is not scoped", async() => {
+        const admin = getAdminFirestore();
+        const exTypeId = "gobal_expense_type";
+        const setupExType = admin.collection("expenseTypes").doc(exTypeId);
+        await setupExType.set({isScoped: false});
+
+        const subTypeId = "global_sub_type";
+        const setupSubType = admin.collection("expenseTypes").doc(exTypeId).collection("expenseTypeSubTypes").doc(subTypeId);
+        await setupSubType.set({isScoped: false});
+
+        const db = getFirestore(myAuth);
+        const testRead = db.collection("expenseTypes").doc(exTypeId).collection("expenseTypeSubTypes").doc(subTypeId);
+        await firebase.assertSucceeds(testRead.get());
+    });
+
+    it("Can't read subTypes if parent expenseType is not scoped and subType is scoped without user", async() => {
+        const admin = getAdminFirestore();
+        const exTypeId = "gobal_expense_type";
+        const setupExType = admin.collection("expenseTypes").doc(exTypeId);
+        await setupExType.set({isScoped: false});
+
+        const subTypeId = "global_sub_type";
+        const setupSubType = admin.collection("expenseTypes").doc(exTypeId).collection("expenseTypeSubTypes").doc(subTypeId);
+        await setupSubType.set({isScoped: true, scopedUsers: [theirId]});
+
+        const db = getFirestore(myAuth);
+        const testRead = db.collection("expenseTypes").doc(exTypeId).collection("expenseTypeSubTypes").doc(subTypeId);
+        await firebase.assertFails(testRead.get());
+    });
+
+    it("Can read subTypes if parent expenseType is scoped with user and subType is not scoped", async() => {
+        const admin = getAdminFirestore();
+        const exTypeId = "gobal_expense_type";
+        const setupExType = admin.collection("expenseTypes").doc(exTypeId);
+        await setupExType.set({isScoped: true, scopedUsers: [myId]});
+
+        const subTypeId = "global_sub_type";
+        const setupSubType = admin.collection("expenseTypes").doc(exTypeId).collection("expenseTypeSubTypes").doc(subTypeId);
+        await setupSubType.set({isScoped: false});
+
+        const db = getFirestore(myAuth);
+        const testRead = db.collection("expenseTypes").doc(exTypeId).collection("expenseTypeSubTypes").doc(subTypeId);
+        await firebase.assertSucceeds(testRead.get());
+    });
+
+    it("Can read subTypes if parent expenseType is scoped with user and subType is scoped with user", async() => {
+        const admin = getAdminFirestore();
+        const exTypeId = "gobal_expense_type";
+        const setupExType = admin.collection("expenseTypes").doc(exTypeId);
+        await setupExType.set({isScoped: true, scopedUsers: [myId]});
+
+        const subTypeId = "global_sub_type";
+        const setupSubType = admin.collection("expenseTypes").doc(exTypeId).collection("expenseTypeSubTypes").doc(subTypeId);
+        await setupSubType.set({isScoped: true, scopedUsers: [myId]});
+
+        const db = getFirestore(myAuth);
+        const testRead = db.collection("expenseTypes").doc(exTypeId).collection("expenseTypeSubTypes").doc(subTypeId);
+        await firebase.assertSucceeds(testRead.get());
+    });
+
+    it("Can't read subTypes if parent expenseType is scoped and user is not scopedUser", async() => {
+        const admin = getAdminFirestore();
+        const exTypeId = "gobal_expense_type";
+        const setupDoc = admin.collection("expenseTypes").doc(exTypeId);
+        await setupDoc.set({isScoped: true, scopedUsers: [theirId]});
+
+        const db = getFirestore(myAuth);
+        const testRead = db.collection("expenseTypes").doc(exTypeId).collection("expenseTypeSubTypes").doc("testDoc");
+        await firebase.assertFails(testRead.get());
+    });
+
+    it("Can create subTypes if parent expense is not scoped", async() => {
+        const admin = getAdminFirestore();
+        const exTypeId = "gobal_expense_type";
+        const setupExType = admin.collection("expenseTypes").doc(exTypeId);
+        await setupExType.set({isScoped: false});
+
+        const db = getFirestore(myAuth);
+        const subTypeId = "global_sub_type";
+        const setupSubType = db.collection("expenseTypes").doc(exTypeId).collection("expenseTypeSubTypes").doc(subTypeId);
+        await firebase.assertSucceeds(setupSubType.set({isScoped: false}));
+    });
+
+    it("Can create subTypes if parent expense is scoped with user", async() => {
+        const admin = getAdminFirestore();
+        const exTypeId = "gobal_expense_type";
+        const setupExType = admin.collection("expenseTypes").doc(exTypeId);
+        await setupExType.set({isScoped: true, scopedUsers: [myId]});
+
+        const db = getFirestore(myAuth);
+        const subTypeId = "global_sub_type";
+        const setupSubType = db.collection("expenseTypes").doc(exTypeId).collection("expenseTypeSubTypes").doc(subTypeId);
+        await firebase.assertSucceeds(setupSubType.set({isScoped: false}));
+    });
+
+    it("Can't create subTypes if parent expense is scoped without user", async() => {
+        const admin = getAdminFirestore();
+        const exTypeId = "gobal_expense_type";
+        const setupExType = admin.collection("expenseTypes").doc(exTypeId);
+        await setupExType.set({isScoped: true, scopedUsers: [theirId]});
+
+        const db = getFirestore(myAuth);
+        const subTypeId = "global_sub_type";
+        const setupSubType = db.collection("expenseTypes").doc(exTypeId).collection("expenseTypeSubTypes").doc(subTypeId);
+        await firebase.assertFails(setupSubType.set({isScoped: false}));
+    });
 
     /***************
      **   USERS   **
@@ -95,8 +216,83 @@ describe("Expense Buddy", () => {
         await firebase.assertFails(testDoc.set({foo: "bar"}));
     });
 
+    it("Can read a user document with the same ID as the user", async() => {
+        const db = getFirestore(myAuth);
+        const testDoc = db.collection("users").doc(myId);
+        await firebase.assertSucceeds(testDoc.get());
+    });
+
+    it("Can't read a user document with a different ID as the user", async() => {
+        const db = getFirestore(myAuth);
+        const testDoc = db.collection("users").doc(theirId);
+        await firebase.assertFails(testDoc.get());
+    });
+
 
     /****************
      **  EXPENSES  **
      ****************/
+    it("Can't create an expense if not logged in", async() => {
+        const expId = "expense_123";
+        const db = getFirestore(null);
+        const testDoc = db.collection("expenses").doc(expId);
+        await firebase.assertFails(testDoc.set({type: "Food"}));
+    });
+
+    it("Can't create an expense if user is logged in but not part of user array", async() => {
+        const expId = "expense_123";
+        const db = getFirestore(myAuth);
+        const testDoc = db.collection("expenses").doc(expId);
+        await firebase.assertFails(testDoc.set({users: [theirId]}));
+    });
+
+    it("Can't create an expense if user is not logged in but part of user array", async() => {
+        const expId = "expense_123";
+        const db = getFirestore(null);
+        const testDoc = db.collection("expenses").doc(expId);
+        await firebase.assertFails(testDoc.set({users: [myId]}));
+    });
+
+    it("Can create an expense if user is logged in and part of user array", async() => {
+        const expId = "expense_123";
+        const db = getFirestore(myAuth);
+        const testDoc = db.collection("expenses").doc(expId);
+        await firebase.assertSucceeds(testDoc.set({users: [myId]}));
+    });
+
+    it("Can't edit an expense if user is not part of user array", async() => {
+        const expId = "expense_123";
+        const db = getFirestore(myAuth);
+        const testDoc = db.collection("expenses").doc(expId);
+        await firebase.assertFails(testDoc.set({users: [theirId]}));
+    });
+
+    it("Can edit an expense if user is part of user array", async() => {
+        const expId = "expense_123";
+        const db = getFirestore(myAuth);
+        const testDoc = db.collection("expenses").doc(expId);
+        await firebase.assertSucceeds(testDoc.set({users: [myId]}));
+    });
+
+    it("Can't read an expense if user is not part of user array", async() => {
+        const admin = getAdminFirestore();
+        const expId = "users_doc";
+        const setupDoc = admin.collection("expenses").doc(expId);
+        await setupDoc.set({users: [theirId]});
+
+        const db = getFirestore(myAuth);
+        const testRead = db.collection("expenses").doc(expId);
+        await firebase.assertFails(testRead.get());
+    });
+
+    it("Can read an expense if user is part of user array", async() => {
+        const admin = getAdminFirestore();
+        const expId = "users_doc";
+        const setupDoc = admin.collection("expenses").doc(expId);
+        await setupDoc.set({users: [myId]});
+
+        const db = getFirestore(myAuth);
+        const testRead = db.collection("expenses").doc(expId);
+        await firebase.assertSucceeds(testRead.get());
+    });
 })
